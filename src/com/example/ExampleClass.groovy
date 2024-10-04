a423ded3fa49ada8c237ea7c93ddf54760a7d6b3
sakis <papatzelosthanashs@gmail.com>
2024-10-04 13:37:18 +0300

    first commit

---
+++ src/com/example/ExampleClass.groovy
@@ -0,0 +1,7 @@
+package com.example
+
+class ExampleClass {
+    static void printMessage(String message) {
+        echo "ExampleClass: ${message}"
+    }
+}
---
+++ vars/example.groovy
@@ -0,0 +1,3 @@
+def call(String message) {
+    echo "Hello from the shared library! Message: ${message}"
+}
