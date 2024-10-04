68479d417c292051f84651142f76e576531bc3f2
PapatzelosThanashs <75451055+PapatzelosThanashs@users.noreply.github.com>
2024-10-04 18:32:32 +0300

    Update newClass.groovy

--- src/com/newexample/newClass.groovy
+++ src/com/newexample/newClass.groovy
@@ -2,7 +2,7 @@ package com.newexample
 
 class newClass {
      void mymethod(String message) {
-     // return  "lets test it: ${message}"
-          echo "it works"     
+      return  "lets test it: ${message}"
+      
     }
 }
